cadc6a2c75e92a919a373a72f8230065fad57aac
GalibIbr <galibiavi4@gmail.com>
2024-04-18 16:14:31 +0300

    created description article

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -13,6 +13,7 @@ import { FreeMode } from "swiper/modules";
 import a1 from "@/public/a1.png";
 import a2 from "@/public/a2.png";
 import Image from "next/image";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
 
 type Props = {
    params: {
@@ -116,6 +117,32 @@ export default function TheMovie({ params: { id } }: Props) {
                </div>
             </SwiperSlide>
          </Swiper>
+         {/* description */}
+         <ul className="description">
+            <li className="tr_active">Описание</li>
+            <li className="">Съемочная группа</li>
+            <li className="">Информация</li>
+         </ul>
+         <p className="descipption_p container">
+            Прекрасная планета Пандорра по-прежнему похожа на рай, где нашел
+            себе приют бывший солдат Салли. Подрастают дети его нового народа,
+            они играют на морских рифах и в подводном царстве. Кроме подростков
+            на’ви на Пандорре растет и земной ребенок Джек Чемпион. Воскресают
+            те, кто умер в былых сражениях: героиня Сигурни Уивер и зловещий
+            полковник. Наконец, и земные корпорации снова предъявляют свои права
+            на богатства Пандорры. Независимость фантастического первобытного
+            рая придется отстаивать всем, кто его населяет.
+         </p>
+         <div className="important">
+            <div className="icon_im">
+               <AiOutlineExclamationCircle />
+            </div>
+            <p>
+               Контент может содержать сцены курения и употребления спиртных
+               напитков. Курение и чрезмерное употребление алкоголя вредит
+               вашему здоровью.
+            </p>
+         </div>
       </div>
    );
 }
